e484f39b325b6e0c9251ff72eef972018be0a2d0
Gregory Norton <gregory.norton@me.com>
2021-03-16 10:17:45 -0400

    Added outline of variables and program components to script

--- project.py
+++ project.py
@@ -3,16 +3,39 @@
 # make sure to import the rom first
 # `python3 -m retro.import "./Rom NoIntro/"`
 
+import numpy as np
 import retro
 import sys
 
 def main(args):
     env = retro.make(game='MegaMan-Nes')
     obs = env.reset()
+#    print(obs, file=sys.stderr)
+    # action[0] is B button, fire
+    # action[1] is ? button, no action
+    # action[2] is ? button, no action
+    # action[3] is ? button, no action
+    # action[4] is ? button, no action
+    # action[5] is ? button, no action
+    # action[6] is left button, move left
+    # action[7] is right button, move right
+    # action[8] is A button, jump
+    action = np.zeros(9)
     while True:
-        obs, rew, done, info = env.step(env.action_space.sample())
+#        action = env.action_space.sample()
+        print(action, file=sys.stderr)
+
+        # obs looks like it might be the screen buffer?
+        # rew ?
+        # done boolean set if the level is completed or levels are out probably
+        # info fetches the state of the predefined variables
+        obs, rew, done, info = env.step(action)
+#        print(obs.size, file=sys.stderr)
+#        print(info, file=sys.stderr)
+        print(rew, file=sys.stderr)
         env.render()
         if done:
+            # end of episode
             return 0
     return 0
 
