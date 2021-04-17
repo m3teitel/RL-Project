615a42f54c3291c97c73051d720032ecf195b508
Gregory Norton <gregory.norton@me.com>
2021-04-17 18:02:46 -0400

    Implemented hopefully running version in keras with plaidml backend

--- project-deepq-keras2.py
+++ project-deepq-keras2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-#https://github.com/keras-rl/keras-rl/blob/master/examples/dqn_cartpole.py
-
+# https://github.com/keras-rl/keras-rl/blob/master/examples/dqn_cartpole.py
+# use plaidml-setup to select target training device
 
 import sys
 import os
@@ -14,11 +14,15 @@ import keras
 from keras import backend as K
 from keras.models import Sequential
 from keras import layers
+from keras.optimizers import Adam
 
 #import rl
+from rl.agents.dqn import DQNAgent
+from rl.policy import EpsGreedyQPolicy
+from rl.memory import SequentialMemory
 
 
-class Network(keras.Model):
+'''class Network(keras.Sequential):
 	_n_features = 2048
 	
 	def __init__(self, input_shape, output_len):
@@ -50,15 +54,50 @@ class Network(keras.Model):
 		h = self.l6(h)
 		if debug: print('output: ', h, file=sys.stderr)
 		
-		return h
+		return h'''
 
 
 def main(args):
 	env = retro.make('MegaMan-Nes', #5000, 0.9,
 		#obs_type=retro.Observations.RAM,
 		use_restricted_actions=retro.Actions.DISCRETE)
-	network = Network(env.observation_space.shape, env.action_space.n)
+	#network = Network(env.observation_space.shape, env.action_space.n)
+	'''network = Sequential()
+	network.add(layers.Input(shape=K.constant(env.observation_space.shape)))
+	network.add(layers.Conv2D(32, 8, strides=4))#, activation='relu'))
+	network.add(layers.Activation('relu'))
+	network.add(layers.Conv2D(64, 4, strides=2))#, activation='relu'))
+	network.add(layers.Activation('relu'))
+	network.add(layers.Conv2D(64, 3, strides=1))#, activation='relu'))
+	network.add(layers.Activation('relu'))
+	network.add(layers.Flatten())
+	network.add(layers.Dense(2048, activation='relu'))
+	network.add(layers.Activation('relu'))
+	network.add(layers.Dense(env.action_space.n))#, activation='relu'))
+	network.add(layers.Activation('linear'))'''
+	
+	network = Sequential([
+		layers.Conv2D(32, 8, strides=4, input_shape=env.observation_space.shape,
+			activation='relu'),
+		layers.Conv2D(64, 4, strides=3, activation='relu'),
+		layers.Conv2D(64, 3, strides=2, activation='relu'),
+		layers.Flatten(),
+		layers.Dense(2048, activation='relu'),
+		layers.Dense(env.action_space.n)])
 	print(network.summary(), file=sys.stderr)
+	
+	memory = SequentialMemory(limit=50000, window_length=1)
+	policy = EpsGreedyQPolicy(eps=0.1)
+	dqn = DQNAgent(model=network, nb_actions=env.action_space.n,
+		memory=memory, nb_steps_warmup=10, policy=policy)
+	dqn.compile(Adam(lr=1e-3), metrics=['mae'])
+	
+	dqn.fit(env, nb_steps=50000, visualize=True, verbose=2)
+	
+	dqn.test(env, nb_episodes=10, visualize=True)
+	
+	
+	
 	
 	return 0
 
